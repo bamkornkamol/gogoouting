ef8f4306dae84d97d9d91755e4112200d595394b
nickanyp <85357272+nickanyp@users.noreply.github.com>
2023-05-17 15:40:07 +0700

    Create price.vue

---
+++ myfrontend/src/views/price.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container w-11/12">
+        <div class="grid grid-cols-2 gap-6">
+            <div class="bg-neutral-400 justify-center content-center items-center flex flex-col p-20 rounded-2xl">
+                <p class="text-8xl mb-12">TAXI</p>
+                <br>
+                <div class="justify-center content-center items-center flex flex-row">
+                    <input id='dest' class="rounded-3xl bg-zinc-200 w-64 h-9 p-6 mr-2" type="text">
+                    <p class="mr-3 text-xl">ไป</p>
+                    <input id='dest' class="rounded-3xl bg-zinc-200 w-64 h-9 p-6 mr-2" type="text">
+                </div>
+                <br>
+                <button onclick="" class="bg-neutral-500 hover:bg-neutral-700 text-white font-bold py-2 px-4 rounded-full">
+                    คิดเลย
+                </button>
+                <br>
+                <p class="text-6xl">100 บาท</p>
+            </div>
+            <div class="bg-neutral-500 justify-center content-center items-center flex flex-col p-20 rounded-2xl">
+                <p class="text-8xl mb-12">BIKE</p>
+                <br>
+                <div class="justify-center content-center items-center flex flex-row">
+                    <input id='dest' class="rounded-3xl bg-zinc-200 w-64 h-9 p-6 mr-2" type="text">
+                    <p class="mr-3 text-xl">ไป</p>
+                    <input id='dest' class="rounded-3xl bg-zinc-200 w-64 h-9 p-6 mr-2" type="text">
+                </div>
+                <br>
+                <button onclick="" class="bg-neutral-400 hover:bg-neutral-700 text-white font-bold py-2 px-4 rounded-full">
+                    คิดเลย
+                </button>
+                <br>
+                <p class="text-6xl">100 บาท</p>
+            </div>
+        </div>
+    </div>
+</template>
