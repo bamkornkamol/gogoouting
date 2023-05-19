ca8e77438f77f65c58b750877d50e9ea54bf07cf
nickanyp <85357272+nickanyp@users.noreply.github.com>
2023-05-20 00:43:44 +0700

    dad

--- myfrontend/src/router.js
+++ myfrontend/src/router.js
@@ -6,13 +6,43 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/calprice',
-    name: 'Home',
+    name: 'Calprice',
     component: () => import('./views/CalPrice.vue')
   },
   {
-    path: '/register',
+    path: '/register_user',
     name: 'Register',
     component: () => import('./views/RegisterUser.vue')
+  },
+  {
+    path: '/register_riders',
+    name: 'Register',
+    component: () => import('./views/RegisterRiders.vue')
+  },
+  {
+    path: '/register_van',
+    name: 'Register',
+    component: () => import('./views/RegisterVan.vue')
+  },
+  {
+    path: '/login_user',
+    name: 'Login',
+    component: () => import('./views/LoginUser.vue')
+  },
+  {
+    path: '/login_riders',
+    name: 'Login',
+    component: () => import('./views/LoginRiders.vue')
+  },
+  {
+    path: '/login_van',
+    name: 'Login',
+    component: () => import('./views/LoginVan.vue')
+  },
+  {
+    path: '/googlemap',
+    name: 'googlemap',
+    component: () => import('./views/GoogleMap.vue')
   },
 ]
 
---
+++ myfrontend/src/views/GoogleMap.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <NavbarAll></NavbarAll>
+        <br>
+        <div class="justify-center content-center items-center flex flex-row">
+            <input id='dest' class="rounded-3xl bg-zinc-200 w-96 h-9 p-6 mr-2" type="text">
+            <p>หาที่นี่ในลาดกระบังให้หน่อย !!</p>
+        </div>
+        <br>
+        <button onclick="place()" class="bg-neutral-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            GO
+        </button>
+
+        <div class="container h-96 w-4/5 ">'
+            <iframe class="h-full w-full" id="map" src="https://maps.google.com/maps?q=&t=&z=15&ie=UTF8&iwloc=&output=embed"
+        frameborder="0" allowfullscreen></iframe>'
+        </div>
+        </div>
+</template>
+
+<script>
+    import NavbarAll from '../components/NavbarAll.vue'
+
+    export default {
+        components: {
+            NavbarAll
+        }
+    }
+</script>
