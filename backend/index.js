25c80c21912cabd732b876bb02f356643c352038
bamkornkamol <88421920+bamkornkamol@users.noreply.github.com>
2023-05-20 22:28:07 +0700

    route backend

--- backend/index.js
+++ backend/index.js
@@ -12,8 +12,20 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 
 // routers
 const indexRouter = require('./routes/index')
+const postUserRouter = require('./routes/post_User')
+const putUserRouter = require('./routes/put_User')
+const delUserRouter = require('./routes/del_User')
+const indexAdminRouter = require('./routes/index_admin')
+const adminRouter = require('./routes/admin')
+const uploadRouter = require('./routes/upload_data')
 
 app.use(indexRouter.router)
+app.use(postUserRouter.router)
+app.use(putUserRouter.router)
+app.use(delUserRouter.router)
+app.use(indexAdminRouter.router)
+app.use(adminRouter.router)
+app.use(uploadRouter.router)
 
 app.listen(3000, () => {
   console.log(`Example app listening at http://localhost:3000`)
